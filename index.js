bcc42a216d62a8341a39a8b9a9b7c502d88faf35
zjeffer <vanhouttetuur@gmail.com>
2018-09-19 20:10:37 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
+
+//The following code is only for Heroku:
+// const express = require('express')
+// const path = require('path')
+// const PORT = process.env.PORT || 5000
+
+// express()
+// .use(express.static(path.join(__dirname, 'public')))
+// .set('views', path.join(__dirname, 'views'))
+// .set('view engine', 'ejs')
+// .get('/', (req, res) => res.render('pages/index'))
+// .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+
 console.log("The bot is starting");
-
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
- 
-server.listen(server_port, server_ip_address, function () {
-  console.log( "Listening on " + server_ip_address + ", port " + server_port )
-});
-
 
 let Twit = require('twit');
 let config = require('./config');
@@ -20,35 +26,43 @@ global.document = document;
 
 let $ = jQuery = require('jquery')(window);
 
+$.getJSON('https://api.spacexdata.com/v2/launches/next', function(data) {
+	execute(data);
+});
 
 setInterval(function(){
 	$.getJSON('https://api.spacexdata.com/v2/launches/next', function(data) {
 		execute(data);
-	}, 6 * 60 * 60 * 1000);
-});
+	});
+}, 6 * 60 * 60 * 1000); //every 6 hours
 
 function execute(data){
-	mission = data.mission_name;
-	UTC = data.launch_date_utc;
-	local = data.launch_date_local;
-	rocket = data.rocket.rocket_name;
-	location = data.launch_site.site_name_long;
+	let mission = data.mission_name;
+	let UTC = data.launch_date_utc;
+	let local = data.launch_date_local;
+	let rocket = data.rocket.rocket_name;
+	let location = data.launch_site.site_name_long;
 
-	ymd = UTC.substring(0,10);
-	hour = UTC.substring(11, 13);
-	minute = UTC.substring(14, 16);
+	let ymd = UTC.substring(0,10);
+	let hour = UTC.substring(11, 13);
+	let minute = UTC.substring(14, 16);
 
-	localymd = local.substring(0,10);
-	localhour = local.substring(11, 13);
-	localminute = local.substring(14, 16);
+	let localymd = local.substring(0,10);
+	let localhour = local.substring(11, 13);
+	let localminute = local.substring(14, 16);
+
 	
-	intervalID = setInterval(post(), 60 * 1000);
-}
+	let intervalID = setInterval(function(){
+		let now = Math.floor(Date.now()/1000); 
+		let unixtime = data.launch_date_unix; 
+		
+		let timeDiff = Math.abs(unixtime - now);
+		let diffDays = timeDiff / (3600 * 24);
+		let diffHours = (diffDays % 1) * 24;
+		let diffMinutes = (diffHours % 1 ) * 60;
+		let diffSeconds = (diffMinutes % 1) * 60;
 
-function post(){
-		let now = Math.floor(Date.now()/1000000); //milliseconds / 1000000 = minutes
-		let unixtime = Math.floor(data.launch_date_unix/1000); //seconds / 1000 = minutes
-		if(now === unixtime - 30){
+		if(diffMinutes === 30 && diffHours === 0 && diffDays === 0){
 			T.post('statuses/update', 
 			{
 				status: 'Next mission: ' + mission + ' using the ' + rocket + ' rocket, launches in 30 minutes. Exact time: ' + ymd + ' at ' + hour + ':' + minute + ' UTC, '
@@ -56,6 +70,8 @@ function post(){
 			})
 			clearInterval(intervalID);
 		}
-		console.log("now=" + now + ", unixtime=" + unixtime);
-}
+		console.log("now=" + new Date(now*1000) + ", unixtime=" + new Date(unixtime*1000) 
+			+ ", difference=" + Math.floor(diffDays) + " days, " + Math.floor(diffHours) + ":" + Math.floor(diffMinutes) + ":" +Math.floor(diffSeconds));
+	}, 60 * 1000); //every minute
 
+}
