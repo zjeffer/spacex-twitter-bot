6827983580d6341f5137a19aca10df1dd2b23fd1
zjeffer <vanhouttetuur@gmail.com>
2021-11-03 12:05:55 +0100

    api v4 branch

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-require('dotenv').config({path: '/home/pi/bot/spacex-twitter-bot/.env'});
+require('dotenv').config({path: '/home/pi/spacex-twitter-bot/.env'});
 const express = require('express');
 const path = require('path');
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 6000;
 
 express()
 .use(express.static(path.join(__dirname, 'public')))
@@ -30,42 +30,88 @@ const { window } = new JSDOM();
 const { document } = (new JSDOM('')).window;
 global.document = document;
 
+
+// the API url
+let url = "https://api.spacexdata.com/v4/launches/query";
+
+// the post options
+let options = {
+    "query": {
+        "upcoming": true
+    },
+    "options": {
+        "select": {
+            "fairings": 0,
+            "links": 0,
+            "tbd": 0,
+            "net": 0,
+            "window": 0,
+            "success": 0,
+            "details": 0,
+            "crew": 0,
+            "ships": 0,
+            "capsules": 0,
+            "payloads": 0,
+            "static_fire_date_utc": 0,
+            "static_fire_date_unix": 0,
+            "auto_update": 0,
+            "launch_library_id": 0,
+            "failures": 0,
+            "upcoming": 0,
+            "cores": 0,
+            "id": 0
+        },
+        "populate": [
+            {
+                "path": "launchpad",
+                "select": "name"
+            },
+            {
+                "path": "rocket",
+                "select": "name"
+            }
+        ],
+        "sort": {
+            "flight_number": "asc"
+        },
+        "limit": 1
+    }
+}
+
 let $ = jQuery = require('jquery')(window);
 
-$.getJSON('https://api.spacexdata.com/v2/launches/next', function(data) {
+$.getJSON(url, options, function(data) {
 	execute(data);
 });
 
 
+
 setInterval(function(){
-	$.getJSON('https://api.spacexdata.com/v2/launches/next', function(data) {
+	$.getJSON(url, options, function(data) {
 		execute(data);
 	});
-}, 6 * 60 * 60 * 1000); //every 6 hours
+}, 1 * 60 * 60 * 1000); //every hour
 
-function execute(data){
+function execute(queryResult){
 	console.log("getJSON function called");
 
-	let mission = data.mission_name;
-	let UTC = data.launch_date_utc;
-	let local = data.launch_date_local;
-	let rocket = data.rocket.rocket_name;
-	let location = data.launch_site.site_name;
+	// get the correct data
+	let data = queryResult.docs[0];
 
-	let ymd = UTC.substring(0,10);
-	let hour = UTC.substring(11, 13);
-	let minute = UTC.substring(14, 16);
+	let mission = data.name;
+	let rocket = data.rocket.name;
+	let location = data.launchpad.name;
+	let launchTime = date.date_unix * 1000; // in millis
 
-	let localymd = local.substring(0,10);
-	let localhour = local.substring(11, 13);
-	let localminute = local.substring(14, 16);
-
-	let localtz = local.substring(19, 22);
-
+	let date = new Date(launchTime);
 	
+	tweet(mission, rocket, location, data);
+	
+	// deprecated
+
+	/*
 	let intervalID = setInterval(function(){
 		let now = Math.floor(Date.now()/1000); 
-		let unixtime = data.launch_date_unix; 
 		
 		let timeDiff = Math.abs(unixtime - now);
 		let diffDays = timeDiff / (3600 * 24);
@@ -104,12 +150,55 @@ function execute(data){
 		}
 		//console.log("now=" + new Date(now*1000) + ", unixtime=" + new Date(unixtime*1000) + ", difference=" + Math.floor(diffDays) + " days, " + Math.floor(diffHours) + ":" + Math.floor(diffMinutes) + ":" + Math.floor(diffSeconds));
 	}, 60 * 1000); //every minute
+
+	*/
+
+}
+
+function tweet(mission, rocket, location, date){
+	console.log(mission, rocket, location, date);
+
+	// every minute:
+	let intervalID = setInterval(() => {
+			
+		// calculate diffs
+		// let diffDays = timeDiff / (3600 * 24); // amount of days
+		// let diffHours = (diffDays % 1) * 24; // amount of hours
+		// let diffMinutes = (diffHours % 1 ) * 60; // amount of minutes
+		// let diffSeconds = (diffMinutes % 1) * 60;
+
+		// current time
+		let now = new Date();
+			
+		// difference between launchtime and current time
+		let timeDiff = launchTime - now;
+		if(timeDiff < 0){
+			clearInterval(intervalID);
+		}
+
+
+		let diffMins = timeDiff / (1000 * 60);
+
+
+		if (Math.floor(diffMins) === (60 * 24 * 7)){
+			// one week
+			tweet(mission, rocket, location, data);
+		} else if(Math.floor(diffMins) === (60 * 24)){
+			// one day
+			tweet(mission, rocket, location, data);
+		} else if (Math.floor(diffMins) === (30)){
+			// 30 minutes
+			tweet(mission, rocket, location, data);
+		} 
+	}, 60 * 1000);
+
+	
 }
 
 function tweeted(err, data, response) {
 	if (err) {
 		console.log("Error when tweeting: " + data + ", response: " + response);
 	} else {
-		console.log("Successful tweet");
+		console.log("Successful tweet: " + data);
 	}
 }
---
+++ start.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+node /home/pi/spacex-twitter-bot/index.js
